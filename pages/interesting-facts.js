e30f77dc7fd0581e58b42d0e6cfc6996f7f87afe
Austin Gae <austingae@gmail.com>
2022-07-17 15:41:03 -0700

    getStaticProps()

--- pages/interesting-facts.js
+++ pages/interesting-facts.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const InterestingFacts = () => {
+const InterestingFacts = ({mixedData}) => {
+  console.log(mixedData);
   return (
-    <div>InterestingFacts</div>
+    <div>
+      {
+        mixedData.map((datum) => {
+          return (
+            <div>
+              <p>{datum.question}</p>
+              <p>{datum.answer}</p>
+            </div>
+          )
+        })
+      }
+    </div>
   )
 }
 
@@ -12,11 +24,10 @@ export async function getStaticProps() {
   let jsonFile = await fetch("https://raw.githubusercontent.com/austingae/coup-d-etat/master/data/interesting-facts-data.json");
   let data = await jsonFile.json();
   let mixedData = data.sort(() => Math.random() - 0.5);
-  console.log(mixedData);
 
   return {
     props: {
-
+      mixedData: mixedData,
     }
   }
 }
@@ -26,5 +37,7 @@ If my understanding of getStaticProps() and getServerSideProps() are correct,
 getStaticProps() fetches the data at build time. If the data has been updated, then the website page will not show it. To show the updated data, must rebuild the website. 
 getServerSideProps() fetches the data at page request. If the data has been updated, then the website page will show it. 
 
-then 
+then...
+If I use getStaticProps(), then whenever I do a page request, the data will not mix up.
+If I use getServerSideProps(), then whenever I do a page request, the data will mix up. 
 */
