185a984f211a9588e149449fe4f6cd88bb984fbc
Austin Gae <austingae@gmail.com>
2022-07-18 22:11:53 -0700

    I decorated with SASS the Interesting Facts Page. The website page for the mobile device looks clean

--- pages/interesting-facts.js
+++ pages/interesting-facts.js
@@ -1,26 +1,48 @@
 import React from 'react'
 import { useState } from 'react'
 
+import styles from '../styles/interesting-facts/interesting-facts.module.css'
+
 const InterestingFacts = ({mixedData}) => {
+  //The index of the mixedData. If the "Next" button clicked, then this state increases by one. If the "Previous" button clicked, then this state decreases by one. 
   let [qAndANumber, setqAndANumber] = useState(0);
+
+  //If the "Read Answer" button clicked, then isReadAnswerClicked = true. If true, then the question's answer will be displayed on the page.
+  let [isReadAnswerClicked, setIsReadAnswerClicked] = useState(false);
+
   return (
-    <div>
-      <h3>{mixedData[qAndANumber].question}</h3>
-      <h3>{mixedData[qAndANumber].answer}</h3>
-      <button 
-        onClick={() => {
-          if (qAndANumber != 0) {
-            setqAndANumber(qAndANumber - 1);
-          }
-        }}>Previous
-      </button>
-      <button onClick={() => {
-          if (qAndANumber != mixedData.length-1) {
-            setqAndANumber(qAndANumber + 1);
-          }
-        }}>Next
-      </button>
-    </div>
+    <>
+      <div className={styles.interestingFactContainer}>
+        <h2 className={styles.interestinFact__title}>Here are some interesting coup facts:</h2>
+        <h3 className={styles.interestingFact__question}>{mixedData[qAndANumber].question}</h3>
+        {isReadAnswerClicked ? <h3 className={styles.interestingFact__answer}>{mixedData[qAndANumber].answer}</h3> : <div className={styles.interestingFact__hideAnswerContainer}></div>}
+
+        <div className={styles.interestingFact__buttonsContainer}>
+          <button className={styles.interestingFact__button} onClick={() => {
+            setIsReadAnswerClicked(true);
+          }}>Read Answer</button>
+
+          <button 
+            className={styles.interestingFact__button}
+            onClick={() => {
+              if (qAndANumber != 0) {
+                setqAndANumber(qAndANumber - 1);
+                setIsReadAnswerClicked(true); //When the previous button is clicked, set isReadAnswerClicked = true because the user already saw the answers to the previous questions, sooo why hide them?
+              }
+            }}>Previous
+          </button>
+          <button 
+          className={styles.interestingFact__button}
+          onClick={() => {
+              if (qAndANumber != mixedData.length-1) {
+                setqAndANumber(qAndANumber + 1);
+                setIsReadAnswerClicked(false); // When the next button is clicked, set isReadAnswerClicked = false or else the next question will already reveal the answer, which is not what the user will want.
+              }
+            }}>Next
+          </button>
+        </div>
+      </div>
+    </>
   )
 }
 
